ae71d27802f30d46d2968b1cf1ddad376bc58eb1
tetraxile-ci <tatrexile@gmail.com>
2023-02-12 20:32:46 +0000

    feat: add snake movement

--- run.py
+++ run.py
@@ -3,10 +3,63 @@
 """
 
 import curses
+import enum
 import time
+from dataclasses import dataclass
 
 
 FPS = 60
+GAME_WIDTH = 20
+GAME_HEIGHT = 20
+
+
+class Direction(enum.IntEnum):
+    """
+        Enum for the directions the snake can travel
+    """
+
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+
+@dataclass
+class Segment:
+    """
+        A segment/cell of the snake
+    """
+    x: int
+    y: int
+
+
+class Snake:
+    """
+        The snake that the player controls using the arrow keys
+    """
+
+    def __init__(self):
+        self.head = Segment(GAME_HEIGHT // 2, GAME_WIDTH // 2)
+
+    def move(self, direction: Direction):
+        """
+            Move the snake's head in a direction, and have its body follow it
+        """
+
+        if direction == Direction.UP:
+            self.head.y -= 1
+        elif direction == Direction.DOWN:
+            self.head.y += 1
+        elif direction == Direction.LEFT:
+            self.head.x -= 1
+        elif direction == Direction.RIGHT:
+            self.head.x += 1
+
+        # clamp snake position to stay within the game window
+        self.head.x = min(self.head.x, GAME_WIDTH - 1)
+        self.head.x = max(self.head.x, 0)
+        self.head.y = min(self.head.y, GAME_HEIGHT - 1)
+        self.head.y = max(self.head.y, 0)
 
 
 def game_loop(screen):
@@ -14,35 +67,44 @@ def game_loop(screen):
         Main game loop, handles all game updates and drawing to the screen
     """
 
-    cursor_x, cursor_y = 0, 0
+    snake = Snake()
+    previous_input = Direction.RIGHT
 
     running = True
     while running:
         frame_start = time.time()
 
+        # clear the screen
         screen.clear()
-        height, width = screen.getmaxyx()
 
+        # read the player input and set the direction accordingly
+        current_input = None
         k = screen.getch()
         if k == ord("q"):
+            # if the player presses q, close the game
             running = False
-        elif k == curses.KEY_DOWN:
-            cursor_y += 1
         elif k == curses.KEY_UP:
-            cursor_y -= 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x += 1
+            current_input = Direction.UP
+        elif k == curses.KEY_DOWN:
+            current_input = Direction.DOWN
         elif k == curses.KEY_LEFT:
-            cursor_x -= 1
+            current_input = Direction.LEFT
+        elif k == curses.KEY_RIGHT:
+            current_input = Direction.RIGHT
 
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width - 1, cursor_x)
+        # if the player didn't input anything, continue moving in the
+        # same direction
+        if current_input is None:
+            current_input = previous_input
 
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height - 1, cursor_y)
+        # apply the movement direction to the snake
+        snake.move(current_input)
+        previous_input = current_input
 
-        screen.move(cursor_y, cursor_x)
+        # move the cursor to the snake position
+        screen.move(snake.head.y, snake.head.x)
 
+        # update the display with what has been drawn
         screen.refresh()
 
         # limit framerate to `FPS`
@@ -57,11 +119,16 @@ def main(screen):
         Main program entrypoint
     """
 
+    # initialize the screen
     screen.clear()
     screen.refresh()
+    # make `getch` non-blocking
     screen.nodelay(1)
 
+    # initialize colors used for the game
+    # text
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    # snake
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)
 
     game_loop(screen)
