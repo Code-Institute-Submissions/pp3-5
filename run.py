eb5c5a401f4cfb77ea855ea54a733bfcb0c1a044
tetraxile-ci <tatrexile@gmail.com>
2023-02-12 17:00:07 +0000

    feat: add basic curses boilerplate

--- run.py
+++ run.py
@@ -1,3 +1,44 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""Module providing TUI (text user interface) functionality"""
+import curses
+
+
+def draw_menu(screen):
+    k = 0
+    cursor_x, cursor_y = 0, 0
+
+    screen.clear()
+    screen.refresh()
+
+    while k != ord("q"):
+        screen.clear()
+        height, width = screen.getmaxyx()
+
+        if k == curses.KEY_DOWN:
+            cursor_y += 1
+        elif k == curses.KEY_UP:
+            cursor_y -= 1
+        elif k == curses.KEY_RIGHT:
+            cursor_x += 1
+        elif k == curses.KEY_LEFT:
+            cursor_x -= 1
+
+        cursor_x = max(0, cursor_x)
+        cursor_x = min(width - 1, cursor_x)
+
+        cursor_y = max(0, cursor_y)
+        cursor_y = min(height - 1, cursor_y)
+
+        screen.move(cursor_y, cursor_x)
+
+        screen.refresh()
+
+        k = screen.getch()
+
+
+def main():
+    """Main program entrypoint"""
+    curses.wrapper(draw_menu)
+
+
+if __name__ == "__main__":
+    main()
